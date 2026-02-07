288deba531cb0efdcee6a69c4ec67d8515c9ff18
John Liao <john76391@gmail.com>
2026-02-07 21:57:11 +0800

    remove demo page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/home";
-import Demo from "./pages/demo";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/demo" element={<Demo />} />
       </Routes>
     </BrowserRouter>
   );
